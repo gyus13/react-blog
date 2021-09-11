30910cc468c43ac9bb3d7ad264ee83349ce89b82
gyus <gyusss13@naver.com>
2021-09-11 14:09:09 +0900

    react2

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    let posts = '맛집';
+    function hundread(){
+        return 100
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style = { {color : 'blue', fontSize : '30px'} }>개발 blog</div>
+      </div>
+        <img src = {logo} />
+        <h4> { posts } </h4>
     </div>
   );
 }
